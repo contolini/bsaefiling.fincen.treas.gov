580a9c9098c95cfdf22463dc55bac0929d346787
Chris Contolini <contolini@users.noreply.github.com>
2023-08-09 11:59:37 -0700

    :rocket:

---
+++ README.md
@@ -0,0 +1,5 @@
+## bsaefiling.fincen.treas.gov
+
+http://gov-proxy.github.io/bsaefiling.fincen.treas.gov/
+
+A clone of the Bank Secrecy Act E-Filing website. Updated nightly.
---
+++ index.js
@@ -0,0 +1,23 @@
+import replace from 'replace-in-file';
+import scrape from 'website-scraper';
+import fs from 'fs';
+
+const options = {
+  urls: ['https://bsaefiling.fincen.treas.gov/main.html'],
+  directory: './dist',
+  urlFilter: (url) => url.startsWith('https://bsaefiling.fincen.treas.gov/'),
+  recursive: true,
+  maxRecursiveDepth: 20,
+};
+
+const result = await scrape(options);
+
+const results = replace.sync({
+  files: './dist/*.html',
+  from: /<\/body>/g,
+  to: '<script>document.querySelectorAll("a[href]").forEach(link => {link.href=link.href.replace(/^https:\\/\\/bsaefiling\\.fincen\\.treas\\.gov\\//, "./")})</script></body>',
+});
+
+fs.copyFile('./dist/index.html', './dist/main.html', (err) => {
+  if (err) throw err;
+});
