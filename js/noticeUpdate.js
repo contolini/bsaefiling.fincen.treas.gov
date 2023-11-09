98a3de46b78e90c49ea495b35617e79f81a93feb
contolini <contolini@users.noreply.github.com>
2023-11-09 00:37:15 +0000

    deploy: 580a9c9098c95cfdf22463dc55bac0929d346787

--- js/noticeUpdate.js
+++ js/noticeUpdate.js
@@ -1,102 +1,22 @@
 var noticeObj = {
-	alertTitle: "UPDATE: New User Interface for Registered BSA E-Filers",
-	newsAlerts: [
-	    /* TEMPLATE for NEW NEWS ALERTS
- 	    {
-	 		date: "", //YYYYMMDD
-			title: "",
-			description: "", //OPTIONAL, short description
-			link: newsItemUrl + "<File name>", //File name
-	    },
-	    */
+        alertTitle: "BSA E-Filing News and Information",
+        newsAlerts: [
+            /* TEMPLATE for NEW NEWS ALERTS
             {
-                        date: "20231016", //YYYYMMDD
-                        title: "FinCEN Provides FBAR Relief to Filers Affected by the Terroristic Action in the State of Israel",
-                        description: "FinCEN announced today that certain individuals and businesses affected by the terroristic action in the State of Israel that are required to file FBARs have until October 7, 2024." +
+                        date: "", //YYYYMMDD
+                        title: "",
+                        description: "", //OPTIONAL, short description
+                        link: newsItemUrl + "<File name>", //File name
+            },
+            {
+                        date: "20210610", //YYYYMMDD
+                        title: "BSA E-Filing Website Modernization (Private Side)",
+                        description: "The BSA E-Filing System's private-side web interface has a new look!" +
                                         "<br>" +
                                         "Click on the title link for more information.",
-                        link: newsItemUrl + "FBAR_Notice_Terroristic_Action_FINAL508.pdf", //File name
+                        link: newsItemUrl + "FinCEN_BSAE-FilingSystem_WebsiteModernizationNotice_PrivateSide_Final.pdf", //File name
             },
-
-            {
-                        date: "20231013", //YYYYMMDD
-                        title: "FinCEN Extends FBAR Deadline for Victims of Recent Natural Disasters",
-                        description: "FinCEN provides FBAR relief to victims of the Hawaii wildfires and Hurricane Idalia in parts of Florida and Georgia; filers have until February 15, 2024, to file." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "FBAR_Notice_Hawaii_Wildfires_Hurricane_Idalia_FINAL508.pdf", //File name
-            },
-
-            {
-                        date: "20230623", //YYYYMMDD
-                        title: "2023 FinCEN E-Filing Survey",
-                        description: "FinCEN has commissioned the CFI Group to conduct a survey to assess customer satisfaction with the BSA E-Filing System." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "2023SurveyNotice.pdf", //File name
-            },
-
-            {
-                        date: "20221209", //YYYYMMDD
-                        title: "FinCEN Further Extends FBAR Deadline for Certain Financial Professionals (12/9/2022)",
-                        description: "FBAR Filing Requirement for Certain Financial Professionals" +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "FinCENNotice2022-1FBARFilingRequirementforCertainFinancialProfessionals12092022.pdf", //File name
-            },
-
-            {
-                        date: "20221006", //YYYYMMDD
-                        title: "FinCEN Provides FBAR Relief to Victims of Recent Specific Natural Disasters",
-                        description: "FinCEN provides FBAR relief to victims of hurricane Fiona in Puerto Rico; hurricane Ian in Florida, North Carolina, and South Carolina; and storms and floods in parts of Alaska; filers have until February 15, 2023 to file." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "NoticeFBARFilingReliefHurricaneFionaHurricaneIanAlaskaStormsOctober2022.pdf", //File name
-            },
-
-            {
-                        date: "20220830", //YYYYMMDD
-                        title: "Production SDTM Server IP Change",
-                        description: "THIS NOTICE APPLIES TO SERVER-TO-SERVER BATCH FILERS ONLY. FinCEN's hosting provider, the Bureau of the Fiscal Service, will be changing the BSA E-Filing System's production SDTM IP on September 7, 2022, at 10:00 PM EST." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "SDTMeFilerNotice-IPChange-Aug30-2022.pdf", //File name
-            },
-
-            {
-                        date: "20220624", //YYYYMMDD
-                        title: "2022 FinCEN E-Filing Survey",
-                        description: "FinCEN has commissioned the CFI Group to conduct a survey to assess customer satisfaction with the BSA E-Filing System." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "2022SurveyNotice.pdf", //File name
-            },
-
-            {
-                        date: "20211213", //YYYYMMDD
-                        title: "FBAR Filing Requirement for Certain Financial Professionals",
-                        description: "The Financial Crimes Enforcement Network (FinCEN) is announcing a further extension of time for certain Report of Foreign Bank and Financial Accounts (FBAR) filings in light of the notice of proposed rulemaking (NPRM) FinCEN issued on March 10, 2016, which proposes..." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "508_FBARSignAuthExtension-Notice2021s.pdf", //File name
-            },
-
-            {
-                        date: "20211005", //YYYYMMDD
-                        title: "FinCEN Provides FBAR Relief to Victims of Recent Natural Disasters",
-                        description: "The Financial Crimes Enforcement Network (FinCEN) today issued a notice extending the filing date of Reports of Foreign Bank and Financial Account (FBARs) for victims of recent natural disasters." +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "FBARFilingRelief2021FINAL.pdf", //File name
-            },
-
- 	    {
-	 		date: "20210610", //YYYYMMDD
-			title: "BSA E-Filing Website Modernization (Private Side)",
-			description: "The BSA E-Filing System's private-side web interface has a new look!" +
-					"<br>" +
-					"Click on the title link for more information.",
-			link: newsItemUrl + "FinCEN_BSAE-FilingSystem_WebsiteModernizationNotice_PrivateSide_Final.pdf", //File name
-	    },
-	]
+            */
+        ]
 };
+
