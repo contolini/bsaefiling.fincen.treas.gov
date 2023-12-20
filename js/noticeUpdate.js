97707827edc9963eca73affdf37db16d16c4357b
contolini <contolini@users.noreply.github.com>
2023-12-20 00:31:54 +0000

    deploy: 580a9c9098c95cfdf22463dc55bac0929d346787

--- js/noticeUpdate.js
+++ js/noticeUpdate.js
@@ -1,22 +1,21 @@
 var noticeObj = {
-        alertTitle: "BSA E-Filing News and Information",
-        newsAlerts: [
-            /* TEMPLATE for NEW NEWS ALERTS
-            {
-                        date: "", //YYYYMMDD
-                        title: "",
-                        description: "", //OPTIONAL, short description
-                        link: newsItemUrl + "<File name>", //File name
-            },
-            {
-                        date: "20210610", //YYYYMMDD
-                        title: "BSA E-Filing Website Modernization (Private Side)",
-                        description: "The BSA E-Filing System's private-side web interface has a new look!" +
-                                        "<br>" +
-                                        "Click on the title link for more information.",
-                        link: newsItemUrl + "FinCEN_BSAE-FilingSystem_WebsiteModernizationNotice_PrivateSide_Final.pdf", //File name
-            },
-            */
-        ]
+	alertTitle: "UPDATE: New User Interface for Registered BSA E-Filers",
+	newsAlerts: [
+	    /* TEMPLATE for NEW NEWS ALERTS
+ 	    {
+	 		date: "", //YYYYMMDD
+			title: "",
+			description: "", //OPTIONAL, short description
+			link: newsItemUrl + "<File name>", //File name
+	    },
+ 	    {
+	 		date: "20210610", //YYYYMMDD
+			title: "BSA E-Filing Website Modernization (Private Side)",
+			description: "The BSA E-Filing System's private-side web interface has a new look!" +
+					"<br>" +
+					"Click on the title link for more information.",
+			link: newsItemUrl + "FinCEN_BSAE-FilingSystem_WebsiteModernizationNotice_PrivateSide_Final.pdf", //File name
+	    },
+	    */
+	]
 };
-
